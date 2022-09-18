abc18cf47dad980bdbbd12d5a14d67f359b44736
rony20191 <ronieresilva18@gmail.com>
2022-09-18 18:09:52 -0300

    adicionando docs ao projeto

--- src/User/user.controller.ts
+++ src/User/user.controller.ts
@@ -1,18 +1,41 @@
-import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiParam } from '@nestjs/swagger/dist';
 import { Request, Response } from 'express';
 
-@Controller()
+@Controller('/user')
+@ApiTags('Users')
 export class UserController {
   listUser = [];
 
-  @Get('user')
+  @Get('/')
+  @ApiResponse({ status: 200, description: 'Sucess' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   findAll(@Res() response: Response) {
     return response.status(200).json(this.listUser);
   }
 
-  @Post('user')
+  @Post('/')
+  @ApiResponse({ status: 200, description: 'Sucess' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   cadastrarUser(@Req() request: Request, @Res() response: Response) {
-    this.listUser.push(request.body)
-    return response.status(200).json("adicionado com sucesso");
+    this.listUser.push(request.body);
+    return response.status(200).json('adicionado com sucesso');
+  }
+
+  @Get('/:id')
+  @ApiParam({ description: 'Id',example: '1', name: 'id'})
+  @ApiResponse({ status: 200, description: 'Sucess' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  findById(@Res() response: Response, @Param('id', ParseIntPipe) id: number) {
+    return response.status(200).json(this.listUser.find((item) => item.id));
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { swaggerSetup } from './docs';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  swaggerSetup(app);
   await app.listen(3000);
 }
 bootstrap();
